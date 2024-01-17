e2759ee678d130edf115ec350dd83f6bf9bb79e6
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-01-17 17:08:31 -0300

    Update Soma.py

--- Estruturas de Repetição/Soma.py
+++ Estruturas de Repetição/Soma.py
@@ -1,3 +1,9 @@
+#Crie um programa no qual o usuário informe 2 números inteiros: a e b. 
+#Para que o programa continue sua execução, verifique se a < b. 
+#Se sim, calcule a soma dos números inteiros no intervalo [a, b]. 
+#Caso contrário, informe uma mensagem de erro.
+
+
 a = int(input('Digite o primeiro número inteiro: \n'))
 b = int(input('Digite o segundo número inteiro: \n'))
 
