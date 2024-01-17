f31df743046c23df10e3ab41effc05a11f324925
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-01-17 17:07:10 -0300

    Update IMC.py

--- Estruturas Condicionais/IMC.py
+++ Estruturas Condicionais/IMC.py
@@ -1,3 +1,17 @@
+# O IMC (Índice de Massa Corporal) é utilizado para avaliar o peso de um indivíduo em relação à sua altura e, como resultado, indica se o indivíduo está com o peso ideal, acima ou abaixo do recomendado. 
+#A tabela a seguir indica a situação de um indivíduo adulto em relação ao seu IMC:
+#IMC Situação
+#Abaixo de 18,5 Abaixo do peso
+#Acima de 18,5 e menor que 25 Peso normal
+#A partir de 25 e menor que 30 Sobrepeso
+#Acima de 30 e menor que 35 Obesidade grau 1
+#Acima de 35 e menor que 40 Obesidade grau 2
+#Acima de 40 Obesidade grau 3
+#Para calcular o IMC, é usada a fórmula IMC = peso / (altura*altura). 
+#Construa um programa no qual um usuário informe seu peso e sua altura.
+#A aplicação deve indicar o IMC calculado e a situação do indivíduo.
+
+
 peso = int(input('Digite seu peso arredondado: \n'))
 altura = float(input('Digite sua altura em m: \n'))
 
