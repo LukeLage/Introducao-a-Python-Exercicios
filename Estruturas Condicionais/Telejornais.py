1c7f9ab6bc847293ba9db0740e7023036ab101c5
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-01-17 17:03:18 -0300

    Update Telejornais.py

--- Estruturas Condicionais/Telejornais.py
+++ Estruturas Condicionais/Telejornais.py
@@ -1,3 +1,11 @@
+#Em sua programação semanal, uma rede de televisão apresenta os seguintes telejornais:
+#• Bom Dia Nação, apresentado por Zé PINHEIRO e Ana Carla ARAÚJO
+#• Jornal Brasileiro, apresentado por Bill BONNER E CARLA VASCONCELOS
+#Crie um programa no qual o usuário informe o sobrenome de um dos apresentadores. 
+#Se o sobrenome informado não estiver na lista acima, o programa deve mostrar a mensagem “Apresentador(a) desconhecido(a).”. 
+#Em caso positivo, o programa deve mostrar o nome do telejornal apresentado pelo apresentador(a).
+
+
 jornais = ['Bom dia, Nação', 'Jornal Brasileiro']
 
 apresentadores = ['Zé Pinheiro', 'Ana Carla Araújo', 'Bill Bonner', 'Carla Vasconcelos']
