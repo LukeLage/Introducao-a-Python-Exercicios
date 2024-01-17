f05d270fdf2de58d88cf4ce48dc7ddd1bf463845
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-01-17 17:04:53 -0300

    Update Black Friday.py

--- Estruturas Condicionais/Black Friday.py
+++ Estruturas Condicionais/Black Friday.py
@@ -1,3 +1,12 @@
+#Na última Black Friday, o gerente de uma loja de perfumes colocou todo o seu estoque em promoção, de acordo com a tabela a seguir:
+#Código Condição de Pagamento Desconto (%)
+#1 À vista (em espécie) 15
+#2 Cartão de débito 10
+#3 Cartão de crédito (vencimento) 5
+#Construa um programa que solicite ao operador do caixa o preço total da venda, bem como a forma de pagamento. 
+#Ao fim, o programa deve informar o valor final a ser pago. 
+
+
 tipos_pagamento = ['Formas de Pagamento: ', '1 - À Vista', '2 - Cartão de Débito', '3 - Cartão de Crédito (Vencimento)']
 
 for pagamento in tipos_pagamento:
