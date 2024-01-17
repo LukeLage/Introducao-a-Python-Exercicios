567e435309d5421f7532bda5283a8dd6f0add29d
LukeLage <100864023+LukeLage@users.noreply.github.com>
2024-01-17 17:10:19 -0300

    Update Caixa Eletronico.py

--- Estruturas de Repetição/Caixa Eletronico.py
+++ Estruturas de Repetição/Caixa Eletronico.py
@@ -1,3 +1,12 @@
+# Imagine um sistema de caixa eletrônico. Construa um programa que receba a senha de um correntista para validar o seu acesso ao sistema. 
+#Considere que a senha fictícia do correntista é 123456. 
+#Considere as seguintes restrições:
+#• quando a senha estiver correta, mostrar a mensagem: “Olá, <SEUNOME>. Seja bem-vindo ao nosso banco!"
+#• quando o usuário errar a senha pela primeira vez,mostrar a mensagem: “Senha incorreta! Você aindatem 2 tentativas.”
+#• se o usuário errar a senha pela segunda vez, mostrar a mensagem: “Senha incorreta! Você ainda tem 1 tentativa.”
+#• se o usuário errar a senha novamente, mostrar a mensagem “Sua senha foi bloqueada! Por favor, dirija-se a um de nossos caixas.” e o programa deve ser encerrado.
+
+
 tentativas = 3
 senha_conta = 123456
 
